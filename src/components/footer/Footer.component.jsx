2bf0e365dc5873647c6e89bab652a97001734f04
Yu <yu@hiya.moe>
2024-10-11 15:32:25 -0600

    Updated text of the footer

--- src/components/footer/Footer.component.jsx
+++ src/components/footer/Footer.component.jsx
@@ -3,8 +3,7 @@ import { COPYRIGHT } from "../../App.config";
 function FooterComponent() {
     return(
         <footer>
-            <p>{ COPYRIGHT }</p>
-            <p>Code with ❤️ by <a target="_blank" href="https://github.com/SummonersWithInsomnia">Summoners with Insomnia</a></p>
+            <p>{ COPYRIGHT }. Code with ❤️ by <a target="_blank" href="https://github.com/SummonersWithInsomnia">Summoners with Insomnia</a></p>
         </footer>
     );
 }
