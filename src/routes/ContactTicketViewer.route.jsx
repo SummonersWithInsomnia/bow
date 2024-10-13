8ff035a1d36e4a7f30cf2358d8288d5e39a2a2a9
Yu <yu@hiya.moe>
2024-10-13 02:30:25 -0600

    Fixed the title of the page

--- src/routes/ContactTicketViewer.route.jsx
+++ src/routes/ContactTicketViewer.route.jsx
@@ -2,7 +2,7 @@ import {APP_NAME} from "../App.config";
 import TicketViewerComponent from "../components/contact-ticket/TicketViewer.component";
 
 function ContactTicketViewerRoute() {
-    document.title = "Contact Ticket Viewer - " + APP_NAME;
+    document.title = "View Contact Tickets - " + APP_NAME;
 
     return (
         <main>
